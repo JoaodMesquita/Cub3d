94e2a9d7beffe15ef50c546246e216c62be029c0
Joao Pedro De Mesquita Fernandes <jpmesquita29085@hotmail.com>
2026-05-12 15:31:03 +0100

    changed function place

--- src/parsing/parsing_map_grid.c
+++ src/parsing/parsing_map_grid.c
@@ -6,7 +6,7 @@
 /*   By: joapedro <joapedro@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 12:40:58 by joapedro          #+#    #+#             */
-/*   Updated: 2026/05/12 14:42:36 by joapedro         ###   ########.fr       */
+/*   Updated: 2026/05/12 15:30:21 by joapedro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,30 @@ int	fill_map_grid_array(t_map *map, int start)
 	}
 	map->grid[j] = NULL;
 	return (start);
+}
+void	check_map_chars(char **map_grid, t_map *map)
+{
+	char	*valid_chars;
+	int		i;
+	int		j;
+	
+	valid_chars = " 01NSWE";
+	i = 0;
+	while(map_grid[i])
+	{
+		j = 0;
+		while (map_grid[i][j])
+		{
+			if(!ft_strchr(valid_chars, map_grid[i][j]))
+				error_free_exit(INVALID_CHAR,map);
+			if(is_player(map_grid[i][j]))
+				map->player_count++;
+			j++;
+		}
+		i++;
+	}
+	if (map->player_count > 1)
+		error_free_exit(DUPLICATE_PLAYER,map);
 }
 
 int	parsing_map_grid(t_map *map, int start)
--- src/parsing/parsing_map_grid_utils.c
+++ src/parsing/parsing_map_grid_utils.c
@@ -6,7 +6,7 @@
 /*   By: joapedro <joapedro@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 15:20:01 by joapedro          #+#    #+#             */
-/*   Updated: 2026/05/12 15:20:50 by joapedro         ###   ########.fr       */
+/*   Updated: 2026/05/12 15:30:13 by joapedro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,4 @@
 int	is_player(char c)
 {
 	return (c == 'N' || c == 'S' || c == 'W' || c == 'E');
-}
-
-void	check_map_chars(char **map_grid, t_map *map)
-{
-	char	*valid_chars;
-	int		i;
-	int		j;
-	
-	valid_chars = " 01NSWE";
-	i = 0;
-	while(map_grid[i])
-	{
-		j = 0;
-		while (map_grid[i][j])
-		{
-			if(!ft_strchr(valid_chars, map_grid[i][j]))
-				error_free_exit(INVALID_CHAR,map);
-			if(is_player(map_grid[i][j]))
-				map->player_count++;
-			j++;
-		}
-		i++;
-	}
-	if (map->player_count > 1)
-		error_free_exit(DUPLICATE_PLAYER,map);
 }
